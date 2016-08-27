92a0e191c0e1719a9da3d8ab4c69b9f889ae3ff9
Eric Liao <rcliao01@gmail.com>
2016-08-27 07:10:14 -0700

    add string utils to demonstrate testing

---
+++ src/main/java/csula/cs4660/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package csula.cs4660.utils;
+
+/**
+ * A quick utility class to demonstrate testing
+ */
+public class StringUtils {
+    public static String reverse(String str) {
+        return new StringBuffer(str).reverse().toString();
+    }
+}
