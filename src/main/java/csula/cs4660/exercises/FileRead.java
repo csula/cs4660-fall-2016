4c7d4411c018f40cc96d1909a4a2f16709ac8ab2
Eric Liao <rcliao01@gmail.com>
2016-08-27 08:58:30 -0700

    add programming exercise

---
+++ src/main/java/csula/cs4660/exercises/FileRead.java
@@ -0,0 +1,40 @@
+package csula.cs4660.exercises;
+
+import java.io.File;
+
+/**
+ * Introduction Java exercise to read file
+ */
+public class FileRead {
+    private int[][] numbers;
+    /**
+     * Read the file and store the content to 2d array of int
+     * @param file read file
+     */
+    public FileRead(File file) {
+        // TODO: read the file content and store content into numbers
+    }
+
+    /**
+     * Read the file assuming following by the format of split by space and next
+     * line. Display the sum for each line and tell me
+     * which line has the highest mean.
+     *
+     * lineNumber starts with 0 (programming friendly!)
+     */
+    public int mean(int lineNumber) {
+        return 0;
+    }
+
+    public int max(int lineNumber) {
+        return 0;
+    }
+
+    public int min(int lineNumber) {
+        return 0;
+    }
+
+    public int sum(int lineNumber) {
+        return 0;
+    }
+}
