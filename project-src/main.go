f90c2c5e7481509d8e0fc740c685edf0fc6c2530
Eric Liao <rcliao01@gmail.com>
2016-11-15 23:01:49 -0800

    fix logic to check dead moves

--- project-src/main.go
+++ project-src/main.go
@@ -19,6 +19,7 @@ func mainLoop(board *[20][30]int) {
 	defer debugTimeUse(start)
 
 	var N, P int
+	var currentX, currentY int
 	fmt.Scan(&N, &P)
 
 	for i := 0; i < N; i++ {
@@ -28,13 +29,49 @@ func mainLoop(board *[20][30]int) {
 		// Y1: starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
 		var X0, Y0, X1, Y1 int
 		fmt.Scan(&X0, &Y0, &X1, &Y1)
+		if i == P {
+			currentX = X1
+			currentY = Y1
+			fmt.Fprintf(os.Stderr, "%v %v\n", X1, Y1)
+		}
 		board[Y1][X1] = i + 1
 	}
+	debugBoard(*board)
 
-	// 		debugBoard(board)
-	time.Sleep(99 * time.Millisecond)
+	var move = getPossibleMoves(*board, currentX, currentY)[0]
 
-	fmt.Println("LEFT") // A single line with UP, DOWN, LEFT or RIGHT
+	fmt.Println(move) // A single line with UP, DOWN, LEFT or RIGHT
+}
+
+func getPossibleMoves(board [20][30]int, x int, y int) []string {
+	var result = []string{}
+	moves := [4]string{"UP", "DOWN", "LEFT", "RIGHT"}
+	for i := 0; i < len(moves); i++ {
+		// choose the move if the move is not a suicide move
+		switch moves[i] {
+		case "UP":
+			if y > 0 && board[y-1][x] == 0 {
+				result = append(result, "UP")
+				break
+			}
+		case "DOWN":
+			if y < len(board)-1 && board[y+1][x] == 0 {
+				result = append(result, "DOWN")
+				break
+			}
+		case "LEFT":
+			if x > 0 && board[y][x-1] == 0 {
+				result = append(result, "LEFT")
+				break
+			}
+		case "RIGHT":
+			if x < len(board[x])-1 && board[y][x+1] == 0 {
+				result = append(result, "RIGHT")
+				break
+			}
+		}
+	}
+	return result
 }
 
 func debugBoard(board [20][30]int) {
